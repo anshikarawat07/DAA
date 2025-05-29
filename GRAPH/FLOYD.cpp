4fd3cfa9a7d1ac949bcd602767c669eaace7bb24
anshikarawat07 <160812154+anshikarawat07@users.noreply.github.com>
2025-05-29 18:58:34 +0530

    Create FLOYD.cpp

---
+++ GRAPH/FLOYD.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define INF 1e9
+
+void floydWarshall(int V, vector<vector<int>>& graph) {
+    vector<vector<int>> dist = graph;
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF)
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++) {
+        if (dist[i][i] < 0) {
+            cout << "Negative weight cycle detected.\n";
+            return;
+        }
+    }
+
+    cout << "Shortest distance matrix:\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    int V;
+    cout << "Enter number of vertices: ";
+    cin >> V;
+
+    vector<vector<int>> graph(V, vector<int>(V));
+
+    cout << "Enter the adjacency matrix (use 1000000000 for INF):\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    floydWarshall(V, graph);
+
+    return 0;
+}
